91c1f3268aea447cb4457bea2954fcd59f4a0b0c
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-03-16 16:05:38 +1200

    void unsorted

--- buble_short/buble_short.cpp
+++ buble_short/buble_short.cpp
@@ -37,4 +37,16 @@ void bubbleSortArray() {        // Prosedur untuk mengurutkan array dengan metod
     }
 }
 
+void unsorted() {
+    cout << endl;               // Output baris kosong
+    cout << "==================================" << endl;  // Output ke layar
+    cout << "Elemen Array yang belum tersusun" << endl;    // Output ke layar
+    cout << "==================================" << endl;  // Output ke layar
+    for (int j = 0; j < n; j++) {                   // Looping dengan j dimulai dari 0 hingga n-1
+        cout << a[j] << endl;                       // Output ke layar
+    }
+    cout << endl;
+}
+
+
 
